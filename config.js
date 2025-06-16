6bea4a2033e602a6f53644d264acb7e8a40c8d9b
abdullah74154 <syedasaduzzamanok@gmail.com>
2025-06-16 14:19:03 +0600

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
 
+// 🔐 তোমার সিক্রেটস + কনফিগালেশন
+const IMGBB_API_KEY = "03c15ae7f944c69ae1313adcc0eb27df"; // তোমার Imgbb key
+const GITHUB_TOKEN = "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"; // তোমার PAT
+const GITHUB_USERNAME = "abdullah74154";
+const REPO_NAME = "Syeds_Fail";
+const JSON_FILE_NAME = "images.json";
+const JSON_FILE_URL = "https://raw.githubusercontent.com/abdullah74154/Syeds_Fail/main/images.json";
